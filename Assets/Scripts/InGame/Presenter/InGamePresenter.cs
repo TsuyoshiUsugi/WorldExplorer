1ab19b0bd068a587295e2cf65d77c869c43c12f8
TsuyosiUsugi <qesulive@gmail.com>
2023-12-08 17:43:54 +0900

    [update] とりあえずカードの上にボタンを置いてカードを選択できるように

---
+++ Assets/Scripts/InGame/Presenter/InGamePresenter.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGamePresenter : MonoBehaviour
+{
+    CardView _cardView;
+    PlayerManager _playerManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
