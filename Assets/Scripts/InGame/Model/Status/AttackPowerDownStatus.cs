e391199d0427a3b5cde8f28b9ed9f9817d0e0b23
TsuyosiUsugi <qesulive@gmail.com>
2023-12-11 01:16:16 +0900

    [update] ターンの効果修正中

--- Assets/Scripts/InGame/Model/Status/AttackPowerDownStatus.cs
+++ Assets/Scripts/InGame/Model/Status/AttackPowerDownStatus.cs
@@ -15,6 +15,10 @@ public class AttackPowerDownStatus : TurnStatus
 
     protected override void ExecuteEffect(Status status)
     {
+        if (status.AttackPower <= 0)
+        {
+            _downPower = 0;
+        }
         status.AddAttackPower(_downPower * -1);
     }
 
@@ -40,10 +44,12 @@ public class BlockPowerUpStatus : TurnStatus
     protected override void ExecuteEffect(Status status)
     {
         status.AddBlockPower(_addBlockPower);
+        Debug.Log($"ブロック力上昇{_addBlockPower}");
     }
 
     protected override void CancelEffect(Status status)
     {
         status.AddAttackPower(_addBlockPower * -1);
+        Debug.Log($"ブロック力上昇状態解除{_addBlockPower}");
     }
 }
