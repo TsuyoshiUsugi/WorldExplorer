cda7c007bd993d8aed505ee099fb6cce5aade709
大森 龍人 <106291438+RyutoOhmori0414@users.noreply.github.com>
2023-12-16 16:21:19 +0900

    [add] OutlineとCutinを作成しました。

---
+++ Assets/Shader/UI/Library/Macro.hlsl
@@ -0,0 +1,8 @@
+#ifndef CUSTOM_EFFECT_UI_OUTLINE_INCLUDED
+#define CUSTOM_EFFECT_UI_OUTLINE_INCLUDED
+
+#define max3(value1, value2, value3) max(max(value1, value2), value3)
+
+#define max4(value1, value2, value3, value4) max(max(value1, value2), max(value3, value4))
+
+#endif
