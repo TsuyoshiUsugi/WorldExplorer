e904f071749b9757334feb727600f3135caca940
TsuyosiUsugi <qesulive@gmail.com>
2023-12-16 13:15:51 +0900

    [update] カードの演出クラスを追加、カードデータに入れる予定

---
+++ Assets/Scripts/InGame/View/CardEffectView/ShinkaCardEffectView.cs
@@ -0,0 +1,16 @@
+using Cysharp.Threading.Tasks;
+
+/// <summary>
+/// 進化カードをプレイしたときの演出処理
+/// </summary>
+public class ShinkaCardEffectView : ICardEffectView
+{
+    /// <summary>
+    /// カードをプレイしたときの演出処理を行う
+    /// </summary>
+    /// <returns></returns>
+    public async UniTask PlayCardEffectView()
+    {
+        
+    }
+}
