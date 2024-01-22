0e6709a108c50f35541f899271a2a45d09c304ec
TsuyosiUsugi <qesulive@gmail.com>
2024-01-22 21:55:47 +0900

    [update] ノードプロセッサを理解中

---
+++ Assets/Scripts/InGame/Model/Enemy/BehaviorTree/Editor/BehaviorTreeEditorWindow.cs
@@ -0,0 +1,23 @@
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using GraphProcessor;
+using UnityEngine.Assertions;
+
+namespace TsuyoshiBehaviorTree
+{
+    public class BehaviorTreeEditorWindow : BaseGraphWindow
+    {
+        protected override void InitializeWindow(BaseGraph graph)
+        {
+            Assert.IsNotNull(graph);
+            var fileName = Path.GetFileNameWithoutExtension(AssetDatabase.GetAssetPath(graph));
+            titleContent = new GUIContent(ObjectNames.NicifyVariableName(fileName));
+            if (graphView == null)
+            {
+                graphView = new BaseGraphView(this);
+            }
+            rootView.Add(graphView);
+        }
+    }
+}
