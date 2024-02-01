c01aa6d35404121cfaa61dcd8a7c884a2b7b9bed
TsuyosiUsugi <qesulive@gmail.com>
2024-02-01 19:48:55 +0900

    [update] デッキセレクト画面の処理をだいたい実装

--- Assets/Scripts/OutGame/DeckSelect/DeckExplain.cs
+++ Assets/Scripts/OutGame/DeckSelect/DeckExplain.cs
@@ -8,14 +8,5 @@ using UnityEngine.UI;
 /// </summary>
 public class DeckExplain : MonoBehaviour
 {
-    [SerializeField] private DeckInfo _deckInfo;
-    [SerializeField] private Text _deckTitle;
-    [SerializeField] private Text _deckExplain;
 
-    public void SetDeckInfo(DeckInfo deckInfo)
-    {
-        _deckInfo = deckInfo;
-        _deckTitle.text = _deckInfo.DeckName;
-        _deckExplain.text = _deckInfo.DeckDescription;
-    }
 }
