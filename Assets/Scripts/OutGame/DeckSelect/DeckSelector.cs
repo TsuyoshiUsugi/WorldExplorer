df114f2748a56395097f7fcbac7f3754c6d9ca6d
TsuyosiUsugi <qesulive@gmail.com>
2024-02-01 19:48:55 +0900

    [update] デッキセレクト画面の処理をだいたい実装

--- Assets/Scripts/OutGame/DeckSelect/DeckSelector.cs
+++ Assets/Scripts/OutGame/DeckSelect/DeckSelector.cs
@@ -10,16 +10,17 @@ using UnityEngine.UI;
 public class DeckSelector : MonoBehaviour
 {
     [SerializeField] private List<DeckInfo> _decks;
-    [SerializeField] private DeckInfo _deckInfo;
     [SerializeField] private Text _deckTitle;
     [SerializeField] private Text _deckExplain;
     [SerializeField] private List<GameObject> _deckObjList;
-    [SerializeField] private int _currentDeckIndex;
+    private int _currentDeckIndex;
+    private DeckInfo _deckInfo;
+    private float _size = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
         _currentDeckIndex = 0;
-        SetDeckInfo(_decks[_currentDeckIndex]);
+        SetDeckDescription(_decks[_currentDeckIndex]);
     }
 
     void Update()
@@ -27,30 +28,41 @@ public class DeckSelector : MonoBehaviour
         //デッキは二種類で上下にデッキの画像が移動する
         if (Input.mouseScrollDelta.y > 0 && _currentDeckIndex != 0)
         {
-            SetDeckInfo(_decks[_currentDeckIndex -= 1]);
+            SetDeckDescription(_decks[_currentDeckIndex -= 1]);
             SetDeckImage(true);
         }
         if (Input.mouseScrollDelta.y < 0 && _currentDeckIndex != _decks.Count - 1)
         {
-            SetDeckInfo(_decks[_currentDeckIndex += 1]);
+            SetDeckDescription(_decks[_currentDeckIndex += 1]);
             SetDeckImage(false);
         }
     }
 
+    /// <summary>
+    /// デッキの画像を上下に移動する
+    /// </summary>
+    /// <param name="isUp"></param>
     void SetDeckImage(bool isUp)
     {
         if (isUp)
         {
             _deckObjList[0].transform.SetAsFirstSibling();
+            _deckObjList[0].transform.localScale *= _size;
+            _deckObjList[1].transform.localScale /= _size;
         }
         else
         {
             _deckObjList[1].transform.SetAsFirstSibling();
+            _deckObjList[1].transform.localScale *= _size;
+            _deckObjList[0].transform.localScale /= _size;
         }
     }
 
-
-    public void SetDeckInfo(DeckInfo deckInfo)
+    /// <summary>
+    /// デッキの説明をセットする
+    /// </summary>
+    /// <param name="deckInfo"></param>
+    void SetDeckDescription(DeckInfo deckInfo)
     {
         _deckInfo = deckInfo;
         _deckTitle.text = _deckInfo.DeckName;
